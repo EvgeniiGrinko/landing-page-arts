5c65cf91cef990f6481567a67c07e401c9560d2f
Evgenii <jenek0907@mail.ru>
2020-12-26 21:06:35 +0300

    made functional for showMoreBtn

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
 'use strict';
@@ -12,6 +13,7 @@ sliders('.feedback-slider-item','horizontal','.main-prev-btn', '.main-next-btn')
 sliders('.main-slider-item','vertical');
 forms();
 mask('[name="phone"]');
-checkTextInputs('[name="name"]')
-checkTextInputs('[name="message"]')
+checkTextInputs('[name="name"]');
+checkTextInputs('[name="message"]');
+showMoreStyles('.button-styles', '.styles-2');
 });
