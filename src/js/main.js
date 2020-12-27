7e71ed4dbe5ca063058b316d8fcb9c63e2fe4141
Evgenii <jenek0907@mail.ru>
2020-12-27 21:48:40 +0300

    accordion with js

--- src/js/main.js
+++ src/js/main.js
@@ -23,5 +23,5 @@ showMoreStyles('.button-styles', '#styles .row');
 calc('#size', '#material', '#options', '.promocode', '.calc-price');
 filter();
 pictureSize('.sizes-block');
-accordion('.accordion-heading', '.accordion-block' )
+accordion('.accordion-heading' )
 });
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,21 +1,46 @@
-const accordion = (triggerSelector, itemsSelector) => {
-    const btns = document.querySelectorAll(triggerSelector),
-        blocks = document.querySelectorAll(itemsSelector);
+const accordion = (triggerSelector) => {
+    const btns = document.querySelectorAll(triggerSelector);
+   
 
-        blocks.forEach(block => {
-            block.classList.add('animated', 'fadeInDown');
-        });
 
-        btns.forEach(btn => {
-            btn.addEventListener('click', function() {
-                if (!this.classList.contains('active')) {
-                    btns.forEach(btn => {
-                        btn.classList.remove('active', 'active-style');
-                        this.classList.add('active', 'active-style');
-                    })
-                }
-            })
+    btns.forEach(btn => {
+        btn.addEventListener('click', function(){
+            
+            if (!this.classList.contains('active-style')) {
+                btns.forEach(btn => {
+                    btn.nextElementSibling.style.maxHeight = '0px';
+                    btn.classList.remove('active-style');
+                    btn.nextElementSibling.classList.remove('active-content');
+                })
+                this.classList.add('active-style');
+                this.nextElementSibling.classList.add('active-content');
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px';
+                
+            } else if (this.classList.contains('active-style')) {
+                btns.forEach(btn => {
+                    btn.nextElementSibling.style.maxHeight = '0px';
+                    btn.classList.remove('active-style');
+                    btn.nextElementSibling.classList.remove('active-content');
+                })
+            } 
         })
+    })
+        // blocks = document.querySelectorAll(itemsSelector);
+
+        // blocks.forEach(block => {
+        //     block.classList.add('animated', 'fadeInDown');
+        // });
+
+        // btns.forEach(btn => {
+        //     btn.addEventListener('click', function() {
+        //         if (!this.classList.contains('active')) {
+        //             btns.forEach(btn => {
+        //                 btn.classList.remove('active', 'active-style');
+        //                 this.classList.add('active', 'active-style');
+        //             })
+        //         }
+        //     })
+        // })
 }
 
 export default accordion;
