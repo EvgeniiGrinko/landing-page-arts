d8d4fa16e644757caeb6b5ee486b4498b482b362
Evgenii <jenek0907@mail.ru>
2020-12-27 23:36:38 +0300

    scroll

---
+++ src/js/modules/scrolling.js
@@ -0,0 +1,61 @@
+const scrolling = (upSelector) => {
+    const upElem = document.querySelector(upSelector);
+    window.addEventListener('scroll', () => {
+        if ( document.documentElement.scrollTop > 1650) {
+            upElem.classList.add('animated', 'fadeIn')
+            upElem.classList.remove('fadeOut')
+        } else {
+            upElem.classList.add('fadeOut')
+            upElem.classList.remove('fadeIn')
+        }
+    })
+    const element = document.documentElement,
+    body = document.body;
+    const calcScroll = () => {
+        upElem.addEventListener('click', function(event) {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+            if(this.hash !== ''){ 
+                event.preventDefault();
+                let hashEleement = document.querySelector(this.hash),
+                hashElementTop = 0;
+
+                while (hashEleement.offsetParent) {
+                    hashElementTop += hashEleement.offsetTop;
+                    hashElementTop = hashEleement.offsetParent;
+
+                }
+                hashElementTop = Math.round(hashElementTop);
+                smoothScroll(hashElementTop, hashElementTop, this.hash)
+            }
+        });
+        const smoothScroll = (from, to, hash) => {
+            let timeInterval = 1;
+            let perScrollTop,
+            speed;
+            if (to > from) {
+                speed = 30;
+            } else {
+                speed = -30;
+            }
+            let move = setInterval(function () {
+                let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+                if (
+                    prevSrollTop === scrollTop || 
+                    (to > from && scrollTop >= to ) || 
+                    (to < from && scrollTop <= to)
+                ) {
+                    clearInterval(move);
+                    history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash)
+
+                } else {
+                    body.scrollTop += speed;
+                    element.scrollTop += speed;
+                    prevSrollTop = scrollTop;
+                }
+            }, timeInterval);
+        }
+    }
+    calcScroll();
+}
+
+export default scrolling;
